c0cc36c69d25181624068f001c61b891b410a5b7
Diego Monti <diegmonti@gmail.com>
2016-05-15 19:24:16 +0200

    Spring template

---
+++ src/test/java/it/polito/ai/IftttCloneApplicationTests.java
@@ -0,0 +1,18 @@
+package it.polito.ai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = IftttCloneApplication.class)
+@WebAppConfiguration
+public class IftttCloneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
