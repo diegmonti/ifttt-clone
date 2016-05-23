4ad2a04e8c0656dbd4d74ef80a78b77a7562c182
Diego Monti <diegmonti@gmail.com>
2016-05-23 20:24:38 +0200

    Update dirs structure

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # ifttt-clone
-
-[![Build Status](https://travis-ci.com/dmm42/ifttt-clone.svg?token=QxTKXYbxuohUoj2qBESw&branch=master)](https://travis-ci.com/dmm42/ifttt-clone)
---
+++ src/main/java/iftttclone/config/RootConfig.java
@@ -0,0 +1,10 @@
+package iftttclone.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("iftttclone")
+public class RootConfig {
+
+}
