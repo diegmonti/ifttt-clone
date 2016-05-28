7bd406b2ca2eb0899515a600c8505658b5f3c0bf
Marco Montani <marco.montani.92@gmail.com>
2016-05-28 17:35:22 +0200

    fix missing IngredientTag

--- src/main/java/iftttclone/channels/FakeTwitterChannel.java
+++ src/main/java/iftttclone/channels/FakeTwitterChannel.java
@@ -8,6 +8,7 @@ import iftttclone.channels.annotation.ActionTag;
 import iftttclone.channels.annotation.ActionFieldTag;
 import iftttclone.channels.annotation.ChannelTag;
 import iftttclone.channels.annotation.IngredientTag;
+import iftttclone.channels.annotation.IngredientsTag;
 import iftttclone.channels.annotation.TriggerTag;
 import iftttclone.channels.annotation.TriggerFieldTag;
 
@@ -15,8 +16,10 @@ import iftttclone.channels.annotation.TriggerFieldTag;
 public class FakeTwitterChannel {
 
 	@TriggerTag(name = "New tweet by a specific user", description = "This trigger fires when a user tweets")
-	@IngredientTag(name="TweetText", description="The text of the tweet", example="Hi there!")
-	@IngredientTag(name="TweetUrl", description="The url of the tweet", example="http://foo.com/123")
+	
+	@IngredientsTag(
+	{ @IngredientTag(name="TweetText", description="The text of the tweet", example="Hi there!"),
+	@IngredientTag(name="TweetUrl", description="The url of the tweet", example="http://foo.com/123") })
 	public Map<String, String> newTweetByUser(@TriggerFieldTag(name = "Username", description = "User to check") String username, Date lastRun) {
 		// Lo scheduler chiama il metodo del trigger passando i field e probabilmente anche l'istante dell'ultima esecuzione
 		// Il trigger ritorna null se non bisogna fare nulla, oppure una mappa (eventualmente vuota) contenente gli ingredienti
