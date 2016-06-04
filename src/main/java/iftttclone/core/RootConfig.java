94ff4438710ed7f9042c730e81b0bba70e0090a6
Diego Monti <diegmonti@gmail.com>
2016-06-04 11:50:40 +0200

    Remove .classpath and add properties file

--- src/main/java/iftttclone/core/RootConfig.java
+++ src/main/java/iftttclone/core/RootConfig.java
@@ -7,9 +7,12 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.mariadb.jdbc.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -22,34 +25,38 @@ import org.springframework.transaction.PlatformTransactionManager;
 @EnableScheduling
 @ComponentScan("iftttclone.core")
 @EnableJpaRepositories("iftttclone.repositories")
+@PropertySource(value = { "classpath:application.properties" })
 public class RootConfig {
-	
+	@Autowired
+	private Environment env;
+
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource ds = new BasicDataSource();
 		ds.setDriverClassName(Driver.class.getName());
-		ds.setUrl("jdbc:mysql://localhost:3306/iftttclone");
-		ds.setUsername("root");
-		ds.setPassword("");
+		ds.setUrl(env.getProperty("jdbc.url"));
+		ds.setUsername(env.getProperty("jdbc.username"));
+		ds.setPassword(env.getProperty("jdbc.password"));
 		return ds;
 	}
-	
+
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
 		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		vendorAdapter.setDatabase(Database.MYSQL);
-		
+
 		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
 		factory.setJpaVendorAdapter(vendorAdapter);
 		factory.setPackagesToScan("iftttclone.entities");
 		factory.setDataSource(dataSource);
-		
+
 		Properties properties = new Properties();
-		properties.put("hibernate.hbm2ddl.auto", "create");
+		properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+		properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
 		factory.setJpaProperties(properties);
 		return factory;
 	}
-	
+
 	@Bean
 	public PlatformTransactionManager transactionManager(EntityManagerFactory factory) {
 		JpaTransactionManager txManager = new JpaTransactionManager();
