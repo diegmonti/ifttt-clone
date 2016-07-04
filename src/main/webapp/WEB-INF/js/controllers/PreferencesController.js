846c8f3cf318d47b7bf7dc6a1f3f50cb58c14de0
Diego Monti <diegmonti@gmail.com>
2016-07-04 15:52:55 +0200

    Password confirmation and channel's table in preferences

--- src/main/webapp/WEB-INF/js/controllers/PreferencesController.js
+++ src/main/webapp/WEB-INF/js/controllers/PreferencesController.js
@@ -12,6 +12,12 @@ iftttclone.controller('PreferencesController', ['$scope', '$rootScope', '$http',
         self.updateProfile = function () {
             var updateUser = {};
             if ($scope.user.password !== undefined) {
+                if ($scope.user.password !== $scope.user.confirm) {
+                    self.success = false;
+                    self.error = true;
+                    $scope.errorMessage = "The provided passwords are not equal.";
+                    return;
+                }
                 updateUser.password = $scope.user.password;
             }
             updateUser.email = $scope.user.email;
@@ -22,15 +28,17 @@ iftttclone.controller('PreferencesController', ['$scope', '$rootScope', '$http',
                 url: 'api/user',
                 data: JSON.stringify(updateUser)
             }).then(function () {
+                self.success = true;
                 self.error = false;
+                $scope.successMessage = "The profile was successfully updated.";
             }, function (response) {
+                self.success = false;
                 self.error = true;
                 $scope.errorMessage = response.data.message;
             });
         };
         self.deactivateChannel = function (channel) {
             console.log('api/channels/' + channel + '/deactivate');
-
             $http.post('api/channels/' + channel + '/deactivate').then(function successCallback(response) {
                 console.log(response);
                 $http.get('api/user').then(function successCallback(response) {
