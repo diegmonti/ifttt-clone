de671d56efcb4aee56db5244c3943a7d16f4ffdf
Diego Monti <diegmonti@gmail.com>
2016-07-05 16:12:03 +0200

    Recipe log pagination

--- src/main/java/iftttclone/entities/RecipeLogEvent.java
+++ src/main/java/iftttclone/entities/RecipeLogEvent.java
@@ -4,5 +4,5 @@ package iftttclone.entities;
  * This enumeration represents the possible events that can happen to a recipe.
  */
 public enum RecipeLogEvent {
-	NEW, CHECK, RUN, ERROR, UPDATE, ACTIVE, INACTIVE
+	CREATED, CHECKED, TRIGGERED, FAILED, UPDATED, ACTIVATED, DEACTIVATED
 }
