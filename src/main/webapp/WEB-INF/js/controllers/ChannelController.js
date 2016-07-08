77694ec3f819f12ffc21a3e36f7f2ee258869643
Diego Monti <diegmonti@gmail.com>
2016-07-08 18:48:25 +0200

    Channel page fixes

--- src/main/webapp/WEB-INF/js/controllers/ChannelController.js
+++ src/main/webapp/WEB-INF/js/controllers/ChannelController.js
@@ -48,4 +48,51 @@ iftttclone.controller('ChannelController', ['$scope', '$rootScope', '$http', '$l
                     console.error(response);
                 });
         };
+
+        self.importRecipe = function (recipeId) {
+            $location.path('/importPublicRecipe/' + recipeId);
+        };
+
+        self.favoriteRecipe = function (recipe, $event) {
+            function errorCallback(response) {
+                console.error(response);
+            }
+
+            if (recipe.favorite === true) {
+                $http.post('api/publicrecipes/' + recipe.id + '/remove').then(function successCallback() {
+                    recipe.favorite = false;
+                    recipe.favorites--;
+                }, errorCallback);
+            } else {
+                $http.post('api/publicrecipes/' + recipe.id + '/add').then(function successCallback() {
+                    recipe.favorite = true;
+                    recipe.favorites++;
+                }, errorCallback);
+            }
+
+            $event.stopPropagation();
+        };
+
+        self.selectRecipe = function (recipe, $event) {
+            $scope.selectedRecipe = recipe;
+            angular.element('#deleteRecipeModalShower').trigger('click');
+            $event.stopPropagation();
+        };
+
+        self.deleteRecipe = function (recipe) {
+            $http({
+                method: 'DELETE',
+                url: 'api/publicrecipes/' + recipe.id
+            }).then(function successCallback() {
+                var i;
+                for (i = 0; i < $scope.recipes.length; i++) {
+                    if ($scope.recipes[i] === recipe) {
+                        $scope.recipes.splice(i, 1);
+                        break;
+                    }
+                }
+            }, function errorCallback(response) {
+                console.error(response);
+            });
+        };
     }]);
